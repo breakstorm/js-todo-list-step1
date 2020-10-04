1e7c2ce031edd9b481ae718d689249834a88f6d0
김휘겸 <breakstorm00@gmail.com>
2020-10-04 15:36:36 +0900

    Template 클래스 메소드 View으로 빼기

--- js/view.js
+++ js/view.js
@@ -23,7 +23,7 @@
         let viewCommands = {
             showEntries : function () {
                 console.log('View.render.showEntries execute', data);
-                self._addItem(data);
+                self._addItem2(data);
             },
             clearNewTodo : function () {
                 console.log('View.render.clearNewTodo execute', );
@@ -34,6 +34,31 @@
     }
     View.prototype._addItem = function (id) {
         this.$todoList.innerHTML = this.template.insert(id);
+    }
+    View.prototype._addItem2 = function (data) {
+        
+        this.$todoList.innerHTML = ((data) => {
+            let view = '';
+            for (let i = 0; i < data.length; i++) {
+                let template = this.template.defaultTemplate;
+                let completed = '';
+                let checked = '';
+                console.log('template', template);
+    
+                if (data[i].completed) {
+                    completed = 'completed';
+                    checked = 'checked';
+                }
+    
+                template = template.replace('{{id}}', data[i].id);
+                template = template.replace('{{title}}', data[i].title);
+                template = template.replace('{{completed}}', data[i].completed);
+                template = template.replace('{{checked}}', data[i].checked);
+    
+                view = view + template;
+            }
+            return view;
+        })(data) 
     }
     exports.app = exports.app || {};
     exports.app.View = View;
